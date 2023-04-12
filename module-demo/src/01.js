0bb6ee3ae180b74a3fde70ad3ac9257a709393f6
heyzeng <270806204@qq.com>
2023-04-12 17:59:50 +0800

    demo

---
+++ module-demo/src/01.js
@@ -0,0 +1,13 @@
+// 两个私有方法
+function sum(a,b) {
+    return a+b
+}
+
+function sub(a,b) {
+    return a-b
+}
+// 定义那些方法被调用
+module.exports = {
+    sum,
+    sub
+}
---
+++ module-demo/src/02.js
@@ -0,0 +1,12 @@
+// 引入01.js的方法
+var m = require('./01.js')
+var sum = m.sub(3,1)
+console.log(sum)
+
+// var m = require('/Users/judezeng/IDEAProject/auth-front/module-demo/src/01.js')
+// //调用方法
+// var v1 = m.sum(1, 2)
+// console.log(v1)
+
+// var v2 = m.sub(3, 1)
+// console.log(v2)
